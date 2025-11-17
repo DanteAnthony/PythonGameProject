d008063b1e27fb849a8e501f103158d4277144b5
DanteAnthony <dg.anthony0604@gmail.com>
2025-11-17 14:14:15 -0800

    Fixing Pygame

--- python_game_project_DA.py
+++ python_game_project_DA.py
@@ -5,11 +5,11 @@ from pygame import *
 pygame.init()
 
 # Screen Setup
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 450
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Python Game')
+pygame.display.set_caption('BeanForm')
 
 # Constants
 CLOCK = pygame.time.Clock()
