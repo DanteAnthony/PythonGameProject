f9aa6bc6eea2abc263c16d71daf26981c9c0e6fa
DanteAnthony <dg.anthony0604@gmail.com>
2025-11-21 14:12:24 -0800

    Added enemy with gravity

--- python_game_project_DA.py
+++ python_game_project_DA.py
@@ -9,7 +9,7 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('BeanForm')
+pygame.display.set_caption('Bean')
 
 # Constants
 CLOCK = pygame.time.Clock()
@@ -23,10 +23,10 @@ FPS = 60
 class Bean(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('img/Beanman128.png').convert_alpha()
+        self.image = pygame.image.load('img/Bean2.png').convert_alpha()
         self.rect = self.image.get_rect(center = [x, y])
-        self.vel = 0
-        self.speed = 8
+        self.vel_y = 0
+        self.speed = 9
         self.on_ground = True
         self.facing_right = True
     
@@ -35,6 +35,59 @@ class Bean(pygame.sprite.Sprite):
         self.apply_gravity()
         self.handle_movement()
         self.handle_jumping()
+
+        # Ground Collision (for now)
+        if self.rect.bottom >= 600:
+            self.rect.bottom = 600
+            self.on_ground = True
+        elif self.rect.bottom < 600:
+            self.on_ground = False
+
+    def handle_movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.flip_sprite(False)
+            self.rect.x -= self.speed
+        if keys[pygame.K_d]:
+            self.flip_sprite(True)
+            self.rect.x += self.speed
+
+    def handle_jumping(self):
+        keys = pygame.key.get_pressed()
+        if self.on_ground == True:
+            if keys[pygame.K_SPACE]:
+                self.on_ground = False
+                self.vel_y = -15
+            else:
+                self.on_ground = True
+
+    def flip_sprite(self, new_direction_right):
+        if new_direction_right and not self.facing_right:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.facing_right = True
+        elif not new_direction_right and self.facing_right:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.facing_right = False
+
+    def apply_gravity(self):
+        self.vel_y += 0.98
+        if self.vel_y > 20:
+            self.vel_y = 20
+        self.rect.y += int(self.vel_y)
+
+class Rect(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/Rect.png').convert_alpha()
+        self.rect = self.image.get_rect(center = [x, y])
+        self.vel = 0
+        self.speed = 9
+        self.on_ground = True
+        self.facing_right = True
+    
+    def update(self):
+        # Self Functions
+        self.apply_gravity()
 
         # Ground Collision (for now)
         if self.rect.bottom >= 600:
@@ -63,7 +116,7 @@ class Bean(pygame.sprite.Sprite):
         if self.on_ground == True:
             if keys[pygame.K_SPACE]:
                 self.on_ground = False
-                self.vel = -10
+                self.vel = -15
             else:
                 self.on_ground = True
 
@@ -76,7 +129,7 @@ class Bean(pygame.sprite.Sprite):
             self.facing_right = False
 
     def apply_gravity(self):
-        self.vel += 0.5
+        self.vel += 0.98
         if self.vel > 20:
             self.vel = 20
         self.rect.y += int(self.vel)
@@ -87,15 +140,22 @@ player_group = pygame.sprite.Group()
 player = Bean(int(SCREEN_WIDTH / 2), 100)
 player_group.add(player)
 
+enemy_group = pygame.sprite.Group()
+
+enemy = Rect(int(SCREEN_WIDTH / 3), 100)
+enemy_group.add(enemy)
+
 # Run Loop
 run = True
 while run:
     
     CLOCK.tick(FPS)
 
-    SCREEN.fill((0, 0, 0))
+    SCREEN.fill((72, 72, 72))
     player_group.draw(SCREEN)
     player_group.update()
+    enemy_group.draw(SCREEN)
+    enemy_group.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
